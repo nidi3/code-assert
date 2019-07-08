c9140813f0f8c4c71b13b961af646d9932e0a471
Stefan Niederhauser <ghuder5@gmx.ch>
2019-07-08 16:32:15 +0200

    travis on windows

--- code-assert/src/test/java/guru/nidi/codeassert/dependency/CycleTest.java
+++ code-assert/src/test/java/guru/nidi/codeassert/dependency/CycleTest.java
@@ -120,7 +120,7 @@ public class CycleTest {
         for (int i = 0; i < message.length() && i < s.length(); i++) {
             if (message.charAt(i) != s.charAt(i)) {
                 System.out.println("%%%% dif at " + i);
-                System.out.println(message.substring(i - 5, 11) + "--" + s.substring(i - 5, 11));
+                System.out.println(message.substring(i - 5, i + 6) + "--" + s.substring(i - 5, i + 6));
             }
         }
         assertEquals(message, s);
